433148695b8dff02655954be26f51f45d91861ab
Pratik Chavhan <133641630+PratikChavhan99@users.noreply.github.com>
2025-01-31 22:10:02 +0530

    Create creds.tf

---
+++ creds.tf
@@ -0,0 +1,15 @@
+provider "aws" { 
+   
+   access_key = "ASIAQKTAFL33ART4SPPT" 
+ 
+   secret_key = "1AJn5B5ASwQ5wjB8f4VIsKNqH7fczhLmhxeW0eO8" 
+ 
+   token = 
+"FwoGZXIvYXdzEM3//////////wEaDC75ELLALv6CqWYs3CK8ATnTB1N2l15G1YyIQUsIDNtxaC5yFa
+ utcIPGJyPQVdmvfHRjPPBFWB3X32l7sUUkB7nIRBY33CNWgoEU4NgnPiFZcAkK5GK206EDk7V2j2/
+ +rZGez4VOhTM8RpaOCihw5C/vSa6CpA/0AXjwN0SHE08O0+yvcr4l5BAm/ACgxbko/HD7F/HIgskn
+ siHTq468JHbU3yhhX/nOHYAgJYqP3JzQJIsrpA2i8eKCbuf4ju+sWYg+RAiHc1skhkFDKPuG+KYGMi11
+ LcwsZszcwRKuiKddCSiWzMuh38y4wIGl6vEsVlKAxxDHfjWxrvmcaLC7zn0=" 
+   region = "us-east-1" 
+ 
+} 
